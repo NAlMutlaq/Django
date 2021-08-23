c83711f147121c385c84f66b4e27192d9bd8b807
MISHAL <mishal@MacBook-Pro-4.local>
2021-08-23 10:35:24 +0300

    code start 0.1

---
+++ eCommerce_project/eCommerce_project/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+from django.conf import settings
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('listings/', include('listings.urls')),
+
+]
