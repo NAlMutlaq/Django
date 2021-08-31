b80e6161ae1a856b0c4ebce8c599464085141ad1
Noura <noura@dev.local>
2021-08-31 16:39:34 +0300

    Update Listings App

--- eCommerce_project/listings/views.py
+++ eCommerce_project/listings/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Listing
 
-from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
 
-
 def index(request):
-    return render(request, 'listings.html')
+    listings = Listing.objects.all()
+    context = {'listings': listings}
+    return render(request, 'listings.html', context)
+
